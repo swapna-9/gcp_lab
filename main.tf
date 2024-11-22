d265d209e5b5c2e31a83ac2ecc1004f691e7e637
swapna-9 <134816143+swapna-9@users.noreply.github.com>
2024-11-21 23:42:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,67 +56,9 @@ resource "google_composer_environment" "airquality-composer" {
   config {
     software_config {
       image_version = "composer-2.9.7-airflow-2.9.3"  # Use a valid image version
-      pypi_packages = {
-        "gs://airquality-mlops-rg/composer_requirements/requirements.txt" = ""
-      }
-      env_variables = {
-        # Your previously defined environment variables
-        LOAD_BUCKET_NAME                 = "airquality-mlops-rg"
-        FOLDER_PATH                      = "api_data/"
-        LOAD_OUTPUT_PICKLE_FILE          = "processed/air_pollution.pkl"
-        BIAS_CHECK_INPUT_PATH            = "processed/air_pollution.pkl"
-        BIAS_CHECK_OUTPUT_PATH           = "processed/resampled_data.pkl"
-        BIAS_CHECK_SENSITIVE_FEATURE     = "month"
-        BIAS_CHECK_THRESHOLD             = "0.2"
-        DATA_SPLIT_BUCKET_NAME           = "airquality-mlops-rg"
-        DATA_SPLIT_INPUT_FILE_PATH       = "processed/resampled_data.pkl"
-        DATA_SPLIT_TRAIN_OUTPUT_FILE_PATH= "processed/train/train_data.pkl"
-        DATA_SPLIT_TEST_OUTPUT_FILE_PATH = "processed/test/test_data.pkl"
-        SCHEMA_STATS_BUCKET_NAME         = "airquality-mlops-rg"
-        SCHEMA_STATS_INPUT_FILE_PATH     = "processed/resampled_data.pkl"
-        SCHEMA_FILE_OUTPUT_PATH          = "processed/Schema.pkl"
-        STATS_FILE_OUTPUT_PATH           = "processed/Stats.pkl"
-        DATA_BUCKET_NAME                 = "airquality-mlops-rg"
-        TRAIN_DATA_PIVOT_INPUT           = "processed/train/train_data.pkl"
-        TRAIN_DATA_PIVOT_OUTPUT          = "processed/train/pivot_data.pkl"
-        TRAIN_DATA_RM_COL_INPUT          = "processed/train/pivot_data.pkl"
-        TRAIN_DATA_RM_COL_OUTPUT         = "processed/train/remove_col_data.pkl"
-        TRAIN_DATA_MISS_VAL_INPUT        = "processed/train/remove_col_data.pkl"
-        TRAIN_DATA_MISS_VAL_OUTPUT       = "processed/train/missing_val_data.pkl"
-        TRAIN_DATA_ANMLY_INPUT          = "processed/train/missing_val_data.pkl"
-        TRAIN_DATA_ANMLY_OUTPUT         = "processed/train/anamoly_data.pkl"
-        TRAIN_DATA_FEA_ENG_INPUT         = "processed/train/anamoly_data.pkl"
-        TRAIN_DATA_FEA_ENG_OUTPUT        = "processed/train/feature_eng_data.pkl"
-        TRAIN_SCHEMA_FILE_PATH           = "processed/train/output_schema.pkl"
-        TRAIN_STATS_FILE_PATH            = "processed/train/output_stats.pkl"
-        TRAIN_DATASET_FILE_PATH          = "processed/train/feature_eng_data.pkl"
-        TEST_DATA_PIVOT_INPUT            = "processed/test/test_data.pkl"
-        TEST_DATA_PIVOT_OUTPUT           = "processed/test/pivot_data.pkl"
-        TEST_DATA_RM_COL_INPUT           = "processed/test/pivot_data.pkl"
-        TEST_DATA_RM_COL_OUTPUT          = "processed/test/remove_col_data.pkl"
-        TEST_DATA_MISS_VAL_INPUT         = "processed/test/remove_col_data.pkl"
-        TEST_DATA_MISS_VAL_OUTPUT        = "processed/test/missing_val_data.pkl"
-        TEST_DATA_ANMLY_INPUT           = "processed/test/missing_val_data.pkl"
-        TEST_DATA_ANMLY_OUTPUT          = "processed/test/anamoly_data.pkl"
-        TEST_DATA_FEA_ENG_INPUT          = "processed/test/anamoly_data.pkl"
-        TEST_DATA_FEA_ENG_OUTPUT         = "processed/test/feature_eng_data.pkl"
-        TEST_SCHEMA_FILE_PATH           = "processed/test/output_schema.pkl"
-        TEST_STATS_FILE_PATH            = "processed/test/output_stats.pkl"
-        TEST_DATASET_FILE_PATH          = "processed/test/feature_eng_data.pkl"
-        # SMTP Configuration
-       #AIRFLOW__SMTP__SMTP_HOST        = "smtp.gmail.com"
-        #AIRFLOW__SMTP__SMTP_PORT        = "587"
-        #AIRFLOW__SMTP__SMTP_USER        = "your_email@gmail.com"
-        #AIRFLOW__SMTP__SMTP_PASSWORD    = "your_app_password"  # Use an app-specific password
-        #AIRFLOW__SMTP__SMTP_MAIL_FROM   = "your_email@gmail.com"
-        #AIRFLOW__SMTP__SMTP_DEFAULT_SENDER = "your_email@gmail.com"
-        #AIRFLOW__SMTP__SMTP_DEFAULT_RECIPIENT = "recipient@example.com"
-        #AIRFLOW__SMTP__SMTP_TLS         = "True"
-        #AIRFLOW__SMTP__SMTP_STARTTLS    = "True"
-        #AIRFLOW__SMTP__SMTP_SSL         = "False"
+      
       }
     }
-  }
 }
 
 # Optional: Set up the network and subnetwork (if required)
